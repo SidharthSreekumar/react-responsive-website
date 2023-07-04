a866cc462b625250a7702888841f1441abebc4f4
Sidharth Sreekumar <sidarth.sreekumar@gmail.com>
2023-07-04 18:09:49 -0400

    Articles, blog, footer and readme updates

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,15 +1,55 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import gpt3Logo from '../../assets/logo.svg';
 import './footer.css';
 
 const Footer = () => (
-  <div className="footer">
-    Footer Component
+  <div className="gpt3__footer section__padding">
+    <div className="gpt3__footer-heading">
+      <h1 className="gradient__text">
+        Do you want to step in to the future before others
+      </h1>
+    </div>
+
+    <div className="gpt3__footer-btn">
+      <p>Request Early Access</p>
+    </div>
+
+    <div className="gpt3__footer-links">
+      <div className="gpt3__footer-links_logo">
+        <img src={gpt3Logo} alt="gpt3_logo" />
+        <p>
+          Sidharth Sreekumar, <br /> All Rights Reserved
+        </p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Links</h4>
+        <p>Overons</p>
+        <p>Social Media</p>
+        <p>Counters</p>
+        <p>Contact</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Company</h4>
+        <p>Terms & Conditions </p>
+        <p>Privacy Policy</p>
+        <p>Contact</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Get in touch</h4>
+        <p>Sidharth Sreekumar</p>
+        <p>123-456-7890</p>
+        <p>
+          <a href="mailto:sidarth.sreekumar@gmail.com">
+            sidarth.sreekumar@gmail.com
+          </a>
+        </p>
+      </div>
+    </div>
+
+    <div className="gpt3__footer-copyright">
+      <p>@2023 GPT-3. All rights reserved.</p>
+    </div>
   </div>
 );
-
-Footer.propTypes = {};
-
-Footer.defaultProps = {};
 
 export default Footer;
