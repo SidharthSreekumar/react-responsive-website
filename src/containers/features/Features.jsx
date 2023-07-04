2e13cb2f87e2e80ee5dade638475f70c62883484
Sidharth Sreekumar <sidarth.sreekumar@gmail.com>
2023-07-04 17:31:28 -0400

    features complete

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,15 +1,41 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import Feature from '../../components/feature/Feature';
 import './features.css';
 
+const featuresData = [
+  {
+    title: 'Improving end distrusts instantly',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur obcaecati assumenda earum maiores ullam dolorum sunt laborum dolores itaque praesentium.',
+  },
+  {
+    title: 'Become the tended active',
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur est sit corporis, voluptates soluta voluptatum enim dolores iste! Officia, reiciendis?",
+  },
+  {
+    title: 'Message or am nothing',
+    text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi a nobis sunt eligendi nesciunt provident hic ut est adipisci esse.',
+  },
+  {
+    title: 'Really boy law county',
+    text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam dolorum perspiciatis esse optio odio voluptatibus voluptatem dolores blanditiis iusto eaque!',
+  },
+];
+
 const Features = () => (
-  <div className="features">
-    Features Component
+  <div className="gpt3__features section__padding" id="features">
+    <div className="gpt3__features-heading">
+      <h1 className="gradient__text">
+        The Future is Now and You Just Need to Realize It. Step into Future
+        Today. & Make it Happen.
+      </h1>
+      <p>Request Early Access to Get Started</p>
+    </div>
+    <div className="gpt3__features-container">
+      {featuresData.map((item, index) => (
+        <Feature title={item.title} text={item.text} key={item.title + index} />
+      ))}
+    </div>
   </div>
 );
-
-Features.propTypes = {};
-
-Features.defaultProps = {};
 
 export default Features;
